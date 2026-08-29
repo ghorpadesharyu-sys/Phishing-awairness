63bcafeb05571494b2492e3d8f1098db221b80cd
ghorpadesharyu-sys <ghorpadesharyu@gmail.com>
2026-08-29 16:40:24 +0530

    Add Run_local.bat for local phishing simulation

---
+++ Run_local.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+echo Starting local phishing-awareness simulation...
+echo Open http://127.0.0.1:8000/index.html in your browser.
+python -m http.server 8000 --directory web
+pause
